76833d8d7d70e4a94b60e2b0a1beb08fdba748d9
mpen <manali.sunil.pen@gmail.com>
2022-01-04 11:18:07 +1100

    Testing

---
+++ db/conn.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://manali:Mana%402127@cluster0.2dgu3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let dbConnection;
+
+module.exports = {
+  connect: (callback) => {
+      client.connect((err, db) => {
+          if (err || !db)
+              return callback(err);
+
+          dbConnection = db.db("clientComments");
+
+          console.log("Connected to MongoDB Atlas");
+
+          callback();
+      });
+  },
+  getDB : () => {
+      return dbConnection;
+  },
+  disconnect : () => {
+      client.close();
+  }
+}
